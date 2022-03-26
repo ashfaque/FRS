be13fe9b7931b04dd54d043acac119bba8e50ae2
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-03-26 19:39:50 +0530

    added users app & updated settings.py & urls.py file of FRS

--- FRS/urls.py
+++ FRS/urls.py
@@ -20,4 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include("users.urls")),
+    path('frsmaster/', include("frsmaster.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
