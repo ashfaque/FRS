b4a36e2d8d2f71c914ebdb6bce47b74c73db7dc9
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-04-07 21:56:36 +0530

    added master app in FRS urls.py

--- FRS/urls.py
+++ FRS/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
     path('frsmaster/', include("frsmaster.urls")),
+    path('master/', include("master.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
