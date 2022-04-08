c1af83342c6144050c2f30c5624cd91236f72069
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-04-08 23:08:31 +0530

    added login template and login view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+	path('login/', views.UserLoginView, name = 'login'),
+	path('login/error/', views.ErrorLoginView, name = 'error_login'),
+	path('register/', views.UserRegisterView, name = 'register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import redirect
+from users.models import UserDetail
 
 # Create your views here.
+
+
+def UserLoginView(request):
+    if request.method == 'POST':
+        email_var = request.POST['email']
+        password_var = request.POST['password']
+        # print(f"email---------->{email_var}\npassword------------>{password_var}")
+        validator = UserDetail.objects.filter(email = email_var, password = password_var).last()
+        # validator = auth.authenticate(email__iexact = email_var, password__iexact = password_var)
+        if validator is None:
+            return redirect('/users/login/error/')
+        else:
+            # https://learndjango.com/tutorials/django-login-and-logout-tutorial
+            return redirect('/users/register/')
+
+    else:
+        return render(request, 'admin_login.html')
+
+
+def ErrorLoginView(request):
+    return render(request, 'error_login.html')
+
+
+def UserRegisterView(request):
+    return render(request, 'register_student.html')
+
+
+
+# ? Logout: https://www.youtube.com/watch?v=geGwkBPcacg, https://www.youtube.com/watch?v=CTrVDi3tt8o
