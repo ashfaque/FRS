d36bf28bba4319050c4e7bec5c313d202118a7c2
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-04-10 13:13:40 +0530

    added frsusermapping table

--- frsmaster/models.py
+++ frsmaster/models.py
@@ -1,3 +1,16 @@
+from pyexpat import model
 from django.db import models
+from users.models import UserDetail
+from django.utils import timezone
 
 # Create your models here.
+
+class FRSUserMapping(models.Model):
+    user = models.ForeignKey(UserDetail, on_delete=models.CASCADE, related_name='fum_user')
+    frs_data = models.TextField(blank=True, null=True)
+
+    def __str__(self):    # ? Shows the details of specific fields if a user prints the 'instance' of this model.
+        return str(self.id)
+
+    class Meta:
+        db_table = 'frs_user_mapping'
--- frsmaster/urls.py
+++ frsmaster/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+    path('apply_frs/', views.FMApplyFRSView, name = 'applyfrs'),
 ]
