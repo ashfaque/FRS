fae2bd77f8c3d21dfac553ab61b3e09446d6c70b
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2022-05-24 18:36:28 +0530

    updated Attendance

--- frsmaster/admin.py
+++ frsmaster/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# @admin.register(FRSUserMapping)
+# class FRSUserMapping(admin.ModelAdmin):
+    # list_display = [field.name for field in FRSUserMapping._meta.fields]
+    # search_fields = ('user__first_name','user__last_name', 'frs_data', 'created_at', 'is_deleted', 'deleted_at', 'deleted_by__first_name', 'deleted_by__last_name')
+
+
+@admin.register(Attendance)
+class Attendance(admin.ModelAdmin):
+    list_display = [field.name for field in Attendance._meta.fields]
+    search_fields = ('user__first_name','user__last_name', 'date', 'is_deleted', 'deleted_at', 'deleted_by__first_name', 'deleted_by__last_name')
--- frsmaster/models.py
+++ frsmaster/models.py
@@ -1,24 +1,31 @@
 from django.db import models
 from users.models import UserDetail
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 
-class FRSUserMapping(models.Model):
-    user = models.ForeignKey(UserDetail, on_delete=models.CASCADE, related_name='fum_user')
-    frs_data = models.TextField(blank=True, null=True)
+# class FRSUserMapping(models.Model):
+#     user = models.ForeignKey(UserDetail, on_delete=models.CASCADE, related_name='fum_user')
+#     frs_data = models.TextField(blank=True, null=True)
+#     created_at = models.DateTimeField(default=timezone.now)
+#     is_deleted = models.BooleanField(default=False)
+#     deleted_at = models.DateTimeField(blank=True, null=True)
+#     deleted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, related_name='fum_deleted_by')
 
-    def __str__(self):    # ? Shows the details of specific fields if a user prints the 'instance' of this model.
-        return str(self.id)
+#     def __str__(self):    # ? Shows the details of specific fields if a user prints the 'instance' of this model.
+#         return str(self.id)
 
-    class Meta:
-        db_table = 'frs_user_mapping'
+#     class Meta:
+#         db_table = 'frs_user_mapping'
 
 
 class Attendance(models.Model):
     user = models.ForeignKey(UserDetail, on_delete=models.CASCADE, related_name='attendance_user')
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     is_deleted= models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, related_name='attendance_deleted_by')
 
     def __str__(self):    # ? Shows the details of specific fields if a user prints the 'instance' of this model.
         return str(self.id)
